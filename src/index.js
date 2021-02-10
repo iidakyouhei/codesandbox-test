ad59ca025779f13a60ab386dcc168c3463638acd
iidakyouhei <68779090+iidakyouhei@users.noreply.github.com>
2021-02-10 22:09:49 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -146,5 +146,31 @@
 // console.log(arr4);
 
 /**
- * mapやfilterを使った配列
+ * mapやfilterを使った配列の処理
  */
+const nameArr = ["田中", "山田", "iida"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(`${index+1}${nameArr[index]}`)
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2)
+// nameArr.map((name, index) => console.log(`${index +1}番目${name}`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) =>{
+//   return num % 2 === 0;
+// })
+
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "iida") {
+    return name;
+  } else {
+    return `${name}です`;
+  }
+});
+console.log(newNameArr);
